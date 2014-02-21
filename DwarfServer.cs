c5756492f0b5e1145036e0af303b564cb42dfe0f
Pedro Rittner <pedro.rittner@outlook.com>
2014-02-21 00:58:52 -0500

    - Changed server implementation to something more sane/class-based

--- DwarfServer.cs
+++ DwarfServer.cs
@@ -9,40 +9,75 @@ namespace DwarfServer
 {
 	public class DwarfServer
 	{
+		const int DEFAULT_PORT_NUM = 9845;
 
-		public DwarfServer ()
+		private IPHostEntry iphost;
+		private TcpListener tcp_server;
+		private TcpClient tcp_client;
+		private NetworkStream network_stream;
+
+        public DwarfServer(int portnum = DEFAULT_PORT_NUM)
 		{
-		}
-
-		static void Main(string[] args) {
-			MemoryStream ms = new MemoryStream ();
-			byte[] inbuffer = new byte[256];
-
-			IPHostEntry iphost = Dns.GetHostEntry (Dns.GetHostName ());
+			// Set this to our actual IP (according to DNS)
+			this.iphost = Dns.GetHostEntry(Dns.GetHostName());
 
 			// Set the server to listen for connections on the Isis default port num + 2
-			TcpListener server = new TcpListener (new IPEndPoint (iphost.AddressList [0], Isis.Isis.ISIS_DEFAULT_PORTNOp + 2));
+			this.tcp_server = new TcpListener(new IPEndPoint(iphost.AddressList[0], portnum));
 
-			Console.WriteLine ("Starting Server on " + iphost.AddressList[0] + ":" + (Isis.Isis.ISIS_DEFAULT_PORTNOp + 2));
-			server.Start ();
+			this.tcp_client = null;
+			this.network_stream = null;
+		}
 
-			//TODO: Write actual event loop for Server
-			Console.WriteLine ("Waiting for client connection...");
-			TcpClient client =  server.AcceptTcpClient ();
+		public void server_start()
+		{
+			Console.WriteLine("Starting Server on " + this.tcp_server.ToString());
 
-			NetworkStream stream = client.GetStream ();
+			//TODO: What to do server is still active. No-op?
+			this.tcp_server.Start();
+		}
 
-			int len;
+		public void wait_for_client(int timeout = 0)
+		{
+			if (timeout == 0 || this.tcp_server.Pending())
+			{
+				// Attempt to get the connection immediately if
+				// 1) We don't care about timeouts (will block)
+				// 2) We have a client already trying to connect
+				this.tcp_client = this.tcp_server.AcceptTcpClient();
+			} else { 
+				throw new NotImplementedException();
+			}
+			
+			this.network_stream = this.tcp_client.GetStream();
+		}
+
+		public string get_message()
+		{
+			byte[] inbuffer = new byte[256];
 
 			byte[] header = new byte[4];
-			stream.Read (header, 0, header.Length);
-			Int32 msglen = System.BitConverter.ToInt32 (header, 0);
+            this.network_stream.Read (header, 0, header.Length);
+			Int32 msglen = System.BitConverter.ToInt32(header, 0);
 
 			inbuffer = new byte[msglen];
-			stream.Read (inbuffer, 0, inbuffer.Length);
+			this.network_stream.Read(inbuffer, 0, inbuffer.Length);
 			string msg = System.Text.Encoding.Unicode.GetString(inbuffer);
-			Console.WriteLine (msg);
+			return msg;
+		}
 
+		static void Main(string[] args)
+		{
+			DwarfServer dwarf_server = new DwarfServer();
+			dwarf_server.server_start();
+
+			Console.WriteLine ("Waiting for client connection...");
+			dwarf_server.wait_for_client();
+			
+			while (true)
+			{
+				Console.WriteLine(dwarf_server.get_message());
+				// dwarf_server.wait_for_client();
+			}
 		}
 	}
 }
