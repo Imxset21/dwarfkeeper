e99df8af7e1329a6aaa33d70a32a744d51df2f19
Aidan <stein.aidan@gmail.com>
2014-04-11 10:57:32 -0400

    - Server data updates now propogate results to all other servers via Isis2 OrderedSend()

--- DwarfServer.cs
+++ DwarfServer.cs
@@ -14,7 +14,7 @@ namespace DwarfServer
 {
     
     delegate void dwarfIsisHandler(DwarfCommand args);
-    delegate void dwarfOpHandler(string args);
+    delegate void dwarfOpHandler(string args, bool local);
 
     //TODO: Add logging of some sort to servers - Hook into Isis logging?
 	public class DwarfServer
@@ -35,6 +35,7 @@ namespace DwarfServer
             dwarfOps.Add(DwarfCode.GET_CHILDREN2, (dwarfOpHandler)getChildren2);
             dwarfOps.Add(DwarfCode.SET_NODE, (dwarfOpHandler)setNode);
             dwarfOps.Add(DwarfCode.GET_ALL, (dwarfOpHandler)getNodeAll);
+            dwarfOps.Add(DwarfCode.EXISTS, (dwarfOpHandler)exists);
         }
 
         private static void defineOpHandlers() {
@@ -42,12 +43,22 @@ namespace DwarfServer
                 (dwarfIsisHandler)delegate(DwarfCommand cmd) {
                     ThreadStart ts =
                         new ThreadStart(() => 
-                            dwarfOps[(DwarfCode)cmd.opCode](cmd.args));
+                            dwarfOps[(DwarfCode)cmd.opCode](cmd.args,local:false));
                     Thread t = new Thread(ts);
                     dwarfGroup.SetReplyThread(t);
                     t.Start();
                 };
             dwarfGroup.AllowClientRequests((int)IsisDwarfCode.OPCODE);
+
+            dwarfGroup.Handlers[(int)IsisDwarfCode.UPDATE] += 
+                (dwarfIsisHandler)delegate(DwarfCommand cmd) {
+                    ThreadStart ts = 
+                        new ThreadStart(() =>
+                            dwarfOps[(DwarfCode)cmd.opCode](cmd.args,local:true));
+                    Thread t = new Thread(ts);
+                    dwarfGroup.SetReplyThread(t);
+                    t.Start();
+                };
         }
 
 		/** Starts the TCP server on localhost with port number.
@@ -83,52 +94,99 @@ namespace DwarfServer
         
         /** Create the node at path - CLI command is "create".
          */
-		private static void create(string args)
+		private static void create(string args, bool local=false)
 		{
             string[] argslst = args.Split();
             if(argslst.Length < 2) {
+                dwarfGroup.Reply("Too few arguments provided");
+                return;
+            }
+            string path = argslst[0];
+            string data = argslst[1];
+            
+            if(!local) {
+                List<string> retlst = new List<string>();
+                dwarfGroup.OrderedQuery(Group.ALL,
+                        (int)IsisDwarfCode.UPDATE,
+                        new DwarfCommand((int)DwarfCode.CREATE, args),
+                        new EOLMarker(),
+                        retlst);
+                Predicate<string> p = delegate(string s) {return s.Equals(path);};
+                if(retlst.TrueForAll(p)) {
+                    dwarfGroup.Reply(path);
+                } else {
+                    string err = string.Format(
+                            "Error: Not all servers created node {0}",
+                            path, data);
+                    dwarfGroup.Reply(err);
+                }
                 return;
             }
 
-            //TODO support ACL inputs
-            //TODO support error messages on bad node adds
-            bool success = nodeSys.addNode(argslst[0], argslst[1]);
+            //TODO support ACL inputs 
+            //TODO support error messages on bad node add
+            bool success = nodeSys.addNode(path, data);
             
             //TODO: send update to rest of group
             if(success) {
-                dwarfGroup.Reply(argslst[0]);
+                dwarfGroup.Reply(path);
+
+                //TODO remove print
                 nodeSys.printTree();
+
             } else {
-                string err = 
-                    string.Format("Error: Failed to create node {0}, with data {1}",
-                        argslst[0], argslst[1]);
+                string err = string.Format(
+                        "Error: Failed to create node {0}, with data {1}",
+                        path, data);
                 dwarfGroup.Reply(err);
             }
 		}
 		
-		private static void delete(string args)
+		private static void delete(string args, bool local=false)
 		{
             string[] argslst = args.Split();
             if(argslst.Length < 1) {
+                dwarfGroup.Reply("Too few arguments provided");
                 return;
             }
-            //TODO: Enable Logging & user feedback
-            bool success = nodeSys.removeNode(argslst[0]);
+            string path = argslst[0];
 
+            if(!local) {
+                List<string> retlst = new List<string>();
+                dwarfGroup.OrderedQuery(Group.ALL,
+                        (int)IsisDwarfCode.UPDATE,
+                        new DwarfCommand((int)DwarfCode.DELETE, args),
+                        new EOLMarker(),
+                        retlst);
+                Predicate<string> p = delegate(string s) {return s.Equals(path);};
+                if(retlst.TrueForAll(p)) {
+                    dwarfGroup.Reply(path);
+                } else {
+                    string err = string.Format(
+                            "Error: Not all servers able to delete node {0}",
+                            path);
+                    dwarfGroup.Reply(err);
+                }
+                return;
+            }
+            
+            //TODO: Enable Logging & user feedback
+            bool success = nodeSys.removeNode(path);
             if(success) {
-                dwarfGroup.Reply(argslst[0]);
+                dwarfGroup.Reply(path);
             } else {
                 string err = 
-                    string.Format("Error: Failed to delete node {0}", argslst[0]);
+                    string.Format("Error: Failed to delete node {0}", path);
                 dwarfGroup.Reply(err);
             }
 		}
 
-		private static void getNode(string args)
+		private static void getNode(string args, bool local=false)
 		{
             //TODO Support watches
             string[] argslst = args.Split();
             if(argslst.Length < 1) {
+                dwarfGroup.Reply(new DwarfStat("Too few arguments provided"));
                 return;
             }
             DwarfStat stat = nodeSys.getNodeInfo(argslst[0]);
@@ -142,37 +200,67 @@ namespace DwarfServer
             }
 		}
 
-		private static void setNode(string args)
+		private static void setNode(string args, bool local=false)
 		{
             string[] argslst = args.Split();
             if(argslst.Length < 2) {
+                dwarfGroup.Reply(new DwarfStat("Too few arguments provided"));
                 return;
             }
-            bool success = nodeSys.setData(argslst[0], argslst[1]);
+            string path = argslst[0];
+            string data = argslst[1];
 
+            if(!local) {
+                List<string> retlst = new List<string>();
+                dwarfGroup.OrderedQuery(Group.ALL,
+                        (int)IsisDwarfCode.UPDATE,
+                        new DwarfCommand((int)DwarfCode.SET_NODE, args),
+                        new EOLMarker(),
+                        retlst);
+                Predicate<string> p = delegate(string s) {return s.Equals(data);};
+                if(retlst.TrueForAll(p)) {
+                    dwarfGroup.Reply(data);
+                } else {
+                    string err = string.Format(
+                            "Error: Not all servers able to set node {0} to {1}",
+                            path, data);
+                    dwarfGroup.Reply(err);
+                }
+                return;
+            }
+            
+            bool success = nodeSys.setData(path, data);
             if(success) {
-                dwarfGroup.Reply(argslst[1]);
+                dwarfGroup.Reply(data);
             } else {
                 String err = string.Format(
                         "Failed to set data to {0} at node {1}",
-                        argslst[0], argslst[1]);
+                        data, path);
                 dwarfGroup.Reply(err);
             }
 		}
 
-		private static void stat(string args)
+		private static void exists(string args, bool local=false)
 		{
-            //TODO Support watches
             string[] argslst = args.Split();
             if(argslst.Length < 1) {
+                dwarfGroup.Reply(new DwarfStat("No Arguments Provided"));
                 return;
             }
+
             DwarfStat stats = nodeSys.getNodeInfo(argslst[0]);
-			throw new NotImplementedException("stat is not implemented.");			
+
+            if(null != stats) {
+                stats.includeData();
+                dwarfGroup.Reply(stats);
+            } else {
+                stats = new DwarfStat(argslst[0] + " does not exist");
+                dwarfGroup.Reply(stats);
+            }
 		}
 
 
-        private static void getChildren(string args) {
+        private static void getChildren(string args, bool local=false) {
             string[] argslst = args.Split();
             if(argslst.Length < 1) {
                 return;
@@ -188,10 +276,11 @@ namespace DwarfServer
             dwarfGroup.Reply(string.Join(",", childs));
         }
 
-		private static void getChildren2(string args)
+		private static void getChildren2(string args, bool local=false)
 		{
             string[] argslst = args.Split();
             if(argslst.Length < 1) {
+                dwarfGroup.Reply("Error: No Arguments Provided");
                 return;
             }
             
@@ -206,16 +295,17 @@ namespace DwarfServer
             dwarfGroup.Reply(stat);
 		}
 
-        private static void getNodeAll(string args) {
-             string[] argslst = args.Split();
+        private static void getNodeAll(string args, bool local=false) {
+            string[] argslst = args.Split();
             if(argslst.Length < 1) {
+                dwarfGroup.Reply(new DwarfStat("Error: No Arguments Provided"));
                 return;
             }
             
             DwarfStat stat = nodeSys.getNodeInfo(argslst[0]);
 
             if(null == stat) {
-                dwarfGroup.Reply("Error: Get Children Failed");
+                dwarfGroup.Reply(new DwarfStat("Error: Get Node Failed"));
                 return;
             }
             stat.includeChildLst();
@@ -224,7 +314,7 @@ namespace DwarfServer
             dwarfGroup.Reply(stat);
        }
 
-        private static void test(string args) {
+        private static void test(string args, bool local=false) {
             Console.WriteLine("TEST || " + args);
             dwarfGroup.Reply("0xDEADWARF-TEST");
         }
@@ -235,7 +325,7 @@ namespace DwarfServer
 		}
 		
 
-        private static void printTree() {
+        private static void printTreeLoop() {
             while (true) {
                 nodeSys.printTree();
                 Thread.Sleep(5000);
@@ -259,6 +349,8 @@ namespace DwarfServer
 
 			Console.WriteLine ("Waiting for client connection...");
 			//dwarfServer.waitForClient();
+            //
+            printTreeLoop();
 			
             IsisSystem.WaitForever();
 		}
